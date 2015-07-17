2a5a3ab87cc1699e43649ec17409ab557d738575
ccguo <389878212@qq.com>
2015-07-17 16:16:22 +0800

    file1

---
+++ SqliteTest/TableViewCell1.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell1.h
+//  SqliteTest
+//
+//  Created by summer on 15/7/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell1 : UITableViewCell
+
+@end
---
+++ SqliteTest/TableViewCell1.m
@@ -0,0 +1,23 @@
+//
+//  TableViewCell1.m
+//  SqliteTest
+//
+//  Created by summer on 15/7/17.
+//
+//
+
+#import "TableViewCell1.h"
+
+@implementation TableViewCell1
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
